045a27513c729ae3bd7c3214353000e3b42b73e9
Garrett Franklin <112950661+gfranklin1@users.noreply.github.com>
2024-06-06 04:01:48 -0400

    Update Trie.java

--- WordHunt Solver/src/WordHuntSolver/Trie.java
+++ WordHunt Solver/src/WordHuntSolver/Trie.java
@@ -4,51 +4,50 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Trie {
-    private Node root;
+	private Node root;
 
-    public Trie() {
-        root = new Node();
-    }
+	public Trie() {
+		root = new Node();
+	}
 
-    public void insert(String word) {
-        Node curr = root;
-        for (char ch : word.toCharArray()) {
-            curr.children.putIfAbsent(ch, new Node());
-            curr = curr.children.get(ch);
-        }
-        curr.isWord = true;
-    }
+	public void insert(String word) {
+		Node curr = root;
+		for (char ch : word.toCharArray()) {
+			curr.children.putIfAbsent(ch, new Node());
+			curr = curr.children.get(ch);
+		}
+		curr.isWord = true;
+	}
 
-    public boolean search(String word) {
-        Node curr = root;
-        for (char ch : word.toCharArray()) {
-            if (!curr.children.containsKey(ch)) {
-                return false;
-            }
-            curr = curr.children.get(ch);
-        }
-        return curr.isWord;
-    }
+	public boolean search(String word) {
+		Node curr = root;
+		for (char ch : word.toCharArray()) {
+			if (!curr.children.containsKey(ch)) {
+				return false;
+			}
+			curr = curr.children.get(ch);
+		}
+		return curr.isWord;
+	}
 
-    public boolean startsWith(String prefix) {
-        Node curr = root;
-        for (char ch : prefix.toCharArray()) {
-            if (!curr.children.containsKey(ch)) {
-                return false;
-            }
-            curr = curr.children.get(ch);
-        }
-        return true;
-    }
+	public boolean startsWith(String prefix) {
+		Node curr = root;
+		for (char ch : prefix.toCharArray()) {
+			if (!curr.children.containsKey(ch)) {
+				return false;
+			}
+			curr = curr.children.get(ch);
+		}
+		return true;
+	}
 
+	protected static class Node {
+		protected Map<Character, Node> children;
+		protected boolean isWord;
 
-    protected static class Node {
-        protected Map<Character, Node> children;
-        protected boolean isWord;
-
-        protected Node() {
-            children = new HashMap<>();
-            isWord = false;
-        }
-    }
+		protected Node() {
+			children = new HashMap<>();
+			isWord = false;
+		}
+	}
 }
